917855e4eb76284f6c4543b6bf59cc32f7df78e2
anthqiu <i@qiushi.xyz>
2024-12-18 14:44:48 -0500

    initial commit

---
+++ app/__init__.py
@@ -0,0 +1,19 @@
+"""Create a Flask app and connect routes.
+"""
+
+# from flask import Flask
+
+# from app.routes import blueprint
+
+
+# def create_app() -> Flask:
+#     """Create a Flask app and connect routes.
+
+#     Returns:
+#         Flask: A Flask app.
+#     """
+
+#     app = Flask(__name__)
+#     app.register_blueprint(project_serialization.bp)
+
+#     return app
